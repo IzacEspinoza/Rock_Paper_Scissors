d250bd5767cd67bb88a3d73b5b95ed5460006885
Izac <izikki11499@gmail.com>
2022-03-28 18:23:27 -0700

    Set up the basic structure of the code

---
+++ script.js
@@ -0,0 +1,39 @@
+//Rock Paper Scissors!
+
+//our variables
+let round = 0;
+let player = playersPick();
+let computer = computersPick();
+let playerScore = 0;
+let computerScore = 0;
+
+
+
+/************ FUNCTIONS ***************/
+
+//the game itself(calls playRound in here, playRound calls the player and computer selects)
+function game(){
+    //play the game until someone makes it to 5 points
+    for(let i = 0; i < 5; i++){
+        //game started, so increase the round number
+        round++;
+        //call the playround function
+    }//end game
+    
+    
+}//end game
+
+//play round
+function playRound(){
+
+}//end playRound
+
+//computers picking
+function computersPick(){
+
+}//end computers
+
+//players picking
+function playersPick(){
+
+}//end players
