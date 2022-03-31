19fb666462d755ce9629bd859c1ee307f8aad619
Izac <izikki11499@gmail.com>
2022-03-30 18:20:58 -0700

    Implemented scoring system, based on computerWon and playerWon's values,  awards player or computer a point if they win the round, no points to anyone if round ended in draw or they didnt choose between rock, paper, scissors

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //Rock Paper Scissors!
 
 //global booleans, to see who won a round, and so the fucntions may access it and change it
-let playerWon;
-let computerWon;
+let playerWon = false;
+let computerWon = false;
 
 //the game itself(calls playRound in here, playRound calls the player and computer selects)
 function game(){
@@ -13,6 +13,8 @@ function game(){
     //scores
     let playerScore = 0;
     let computerScore = 0;
+
+    
 
     //play 5 games, at the end see who has most points
     while(round < 5){
@@ -31,6 +33,27 @@ function game(){
          give points to who won a round
          */
 
+         //if computer won round, give them a point and display their score
+         if(computerWon){
+             //give them a point
+             computerScore++;
+             //print their current score
+             console.log("Computer +" + computerScore);
+         }
+         //if player won the round, give them a point and display their score
+         else if(playerWon){
+             //give player a point
+             playerScore++;
+             //print players current score
+             console.log("Player +" + playerScore);
+         }
+         //if whatever else happens, print this
+         else{
+             console.log("No points awarded");
+         }
+
+         //print final  score
+         console.log("Computer: " + computerScore + ", Player: " + playerScore);
         
         
     }//end gameplay loop
@@ -50,49 +73,72 @@ function playRound(playerPick, computerPick){
         //output that paper beats rock
         console.log("Computers Paper beats Players Rock!");
         
-        //computer won, so computerWon is TRUE
+        //computer won, so computerWon is TRUE, playerWon is false;
         computerWon = true;
+        playerWon = false;
     }
     //if user picked rock and computer picked scissors : 
     else if(playerPick == 'rock' && computerPick == 'scissors'){//player win
         //output that rock beats paper
         console.log("Players Rock beats Computers Scissors!");
+
+        //player won, so playerWon is TRUE, computerWon is now false
+        playerWon = true;
+        computerWon = false;
     }
     //if user picked scissors and computer picked paper :
     else if(playerPick == 'scissors' && computerPick == 'paper'){//player win
         //output scissors beats paper
         console.log("Players Scissors beat Computers Paper!");
+
+        //player won, so playerWon is TRUE, computerWon is now false
+        playerWon = true;
+        computerWon = false;
     }
     //if user picked paper and computer picked scissors :
     else if(playerPick == 'paper' && computerPick == 'scissors'){//computer win
         //output scissors beat paper
         console.log("Computers Scissors beat Players Paper!");
 
-        //computer won, so computerWon is TRUE
+        //computer won, so computerWon is TRUE, playerWon is now false
         computerWon = true;
+        playerWon = false;
     }
     //if user picked scissors and computer picked rock : 
     else if(playerPick == 'scissors' && computerPick == 'rock'){//computer win
         //output rock beats scissors
         console.log("Computers Rock beats Players Scissors!");
 
-        //computer won, so computerWon is TRUE
+        //computer won, so computerWon is TRUE, playerWon is now false
         computerWon = true;
+        playerWon  = false;
     }
     //if user picked paper and computer picked rock
     else if(playerPick == 'paper' && computerPick == 'rock'){//player win
         //out put paper beats rock
         console.log("Players Paper beats Computers Rock!");
+
+        //player won, so playerWon is TRUE, computerWon is now false;
+        playerWon = true;
+        computerWon = false;
     }
     //if user and computer both picked the same thing
     else if(playerPick == computerPick && computerPick == playerPick){//draw
         //both picked the same thing
         console.log("You both picked " + playerPick);
+
+        //nobody won, so neither is true
+        computerWon = false;
+        playerWon = false;
     }
     //if whatever they put was not an option, or something else happen
     else{
         //tell they fucked up
         console.log("You fucked up.  Choose between rock, paper, or scissors fool!");
+
+        //playerWon and computerWon are false, because we couldnt even get that far!
+        playerWon = false;
+        computerWon = false;
     }
 
 
