f65544d9048b549612cc522c7e1bb3d36cb21e6d
Izac <izikki11499@gmail.com>
2022-04-23 18:02:37 -0700

    MAJOR CHANGES, Ui is done tho

--- script.js
+++ script.js
@@ -1,154 +1,70 @@
 //Rock Paper Scissors!
 
-//global booleans, to see who won a round
-let playerWon = false;
-let computerWon = false;
+//player and computers scores
+let playerScore = 0
+let computerScore = 0
 
-//grab our div 'results' 
-const resultsContainer = document.querySelector('.results');
-const choices = document.createElement('p');
-resultsContainer.appendChild(choices);
+//ref our buttons
+const buttons = document.querySelectorAll('input')
 
-//List of buttons to get the users input/answer
-const buttons = document.querySelectorAll('button');
-// we use the .forEach method to iterate through each button
-buttons.forEach((button) => {
+//generate random pick for computers answer
+function computerPlay() {
+    let choices = ['rock', 'paper', 'scissors']
+    return choices[Math.floor(Math.random() * choices.length)]
+}
 
-    // and for each one we add a 'click' listener
-    button.addEventListener('click', () => {
+//disables buttons after each game
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
 
-        //computers rando pick, for every button click
-        let comps = computersPick();
+//play a round, using the users selected button
+function playRound(playerSelection) {
+    let computerSelection = computerPlay()
+    let result = ""
 
-        //if the buttons we click has THIS id
-        if(button.id == 'rock'){
-            //call playRound with choice
-            console.log("You picked Rock, Computer picked " + comps);
-            choices.textContent = "You picked " + button.id + ", Computer picked " + comps;
+    //if the players choice won over the computers,
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
+        //give the player a point, and display their choice compared to computers
+        playerScore += 1
+        result = ('You win! ' + playerSelection + ' beats ' + computerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
 
-
-            playRound('rock', comps);  
+        //if user gets 5 points, they won, let them play again
+        if (playerScore == 5) {
+            result += '<br><br>You won the game! Reload the page to play again'
+            disableButtons()
         }
-        else if(button.id == 'paper'){
-            //call playRound with choice
-            console.log("You picked Paper, Computer picked " + comps);
-            choices.textContent = "You picked " + button.id + ", Computer picked " + comps;
-
-            playRound('paper', comps); 
+    }
+    //if user and computer chose the same thing its a tie
+    else if (playerSelection == computerSelection) {
+        result = ('It\'s a tie. You both chose ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+    }
+    //anything else, the computer must have won, so they get a point
+    else {
+        computerScore += 1
+        result = ('You lose! ' + computerSelection + ' beats ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+        //if the computer get 5 points, they win 
+        if (computerScore == 5) {
+            result += '<br><br>I won the game! Reload the page to play again'
+            disableButtons()
         }
-        else if(button.id == 'scissors'){
-            //call playRound with choice
-            console.log("You picked scissors, Computer picked " + comps);
-            choices.textContent = "You picked " + button.id + ", Computer picked " + comps;
-
-            playRound('scissors', comps);
-                
-        }
-            
-        });
-    });//end event listener
-
-//play round(pretty much compare the players and computers choices, see who won the round)
-function playRound(playerPick, computerPick){
-    
-    /*********compare the players and computers answers*********/
-
-    //if user picked rock and computer picked paper :
-    if(playerPick.toUpperCase() == 'ROCK' && computerPick.toUpperCase() == 'PAPER'){//computer win
-        //output that paper beats rock
-        console.log("Computer +1pt");
-        
-        //computer won, so computerWon is TRUE, playerWon is false;
-        computerWon = true;
-        playerWon = false;
-    }
-    //if user picked rock and computer picked scissors : 
-    else if(playerPick.toUpperCase() == 'ROCK' && computerPick.toUpperCase() == 'SCISSORS'){//player win
-        //output that rock beats paper
-        console.log("Player +1pt");
-
-        //player won, so playerWon is TRUE, computerWon is now false
-        playerWon = true;
-        computerWon = false;
-    }
-    //if user picked scissors and computer picked paper :
-    else if(playerPick.toUpperCase() == 'SCISSORS' && computerPick.toUpperCase() == 'PAPER'){//player win
-        //output scissors beats paper
-        console.log("Players +1pt");
-
-        //player won, so playerWon is TRUE, computerWon is now false
-        playerWon = true;
-        computerWon = false;
-    }
-    //if user picked paper and computer picked scissors :
-    else if(playerPick.toUpperCase() == 'PAPER' && computerPick.toUpperCase() == 'SCISSORS'){//computer win
-        //output scissors beat paper
-        console.log("Computer +1pt");
-
-        //computer won, so computerWon is TRUE, playerWon is now false
-        computerWon = true;
-        playerWon = false;
-    }
-    //if user picked scissors and computer picked rock : 
-    else if(playerPick.toUpperCase() == 'SCISSORS' && computerPick.toUpperCase() == 'ROCK'){//computer win
-        //output rock beats scissors
-        console.log("Computer +1pt");
-
-        //computer won, so computerWon is TRUE, playerWon is now false
-        computerWon = true;
-        playerWon  = false;
-    }
-    //if user picked paper and computer picked rock
-    else if(playerPick.toUpperCase() == 'PAPER' && computerPick.toUpperCase() == 'ROCK'){//player win
-        //out put paper beats rock
-        console.log("Players +1pt");
-
-        //player won, so playerWon is TRUE, computerWon is now false;
-        playerWon = true;
-        computerWon = false;
-    }
-    //if user and computer both picked the same thing
-    else if(playerPick.toUpperCase() == computerPick.toUpperCase() && computerPick.toUpperCase() == playerPick.toUpperCase()){//draw
-        //both picked the same thing
-        console.log("Tie");
-
-        //nobody won, so neither is true
-        computerWon = false;
-        playerWon = false;
-    }
-    //if whatever they put was not an option, or something else happen
-    else{
-        //tell they fucked up
-        alert("I dont know how but you fucked up.....");
-
-        //playerWon and computerWon are false, because we couldnt even get that far!
-        playerWon = false;
-        computerWon = false;
     }
 
+    //display to the DOM what the rounds result wass
+    document.getElementById('result').innerHTML = result
+    return
+}
 
-}//end playRound
-
-//computers picking, uses random number to spit out a random answer
-function computersPick(){
-
-    //array of choices for comp to choose
-    const moves = ["rock", "paper", "scissors"];
-
-    //computers random choice
-    let computerPick = "Random";
-
-    //boundaries for the random number(first index of 'moves' array to the last index of it)
-    let min = 0; max = 2;
-    //get a random number, this line pretty much makes it so only 1-3 are included, will not return a 0, giving us an undefined
-    let rando = Math.floor(Math.random() * (max - min + 1) + min);
-
-    //get a random index from the 'moves' array, to get a random answer
-    computerPick = moves[rando];
-
-    //return the random answer
-    return computerPick;
-
-}//end computers
-
-
+//add an event listener for each button, on click, call the playRound()
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        playRound(button.value)
+    })
+})
